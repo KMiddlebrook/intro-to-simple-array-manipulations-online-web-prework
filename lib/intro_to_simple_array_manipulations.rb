03bc8d4c2d420c4f4ab224f4d818f0cbf78b2bb9
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2018-12-19 17:18:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+require "pry"
+def using_push
+  binding.pry
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+    next_country = "Niger"
+    countries_in_western_africa.push(next_country)
+end
+
+def using_unshift
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+end
+
+def using_pop
+  
+end
+
+def pop_with_args
+  
+end
+
+def using_shift
+  
+end
+
+using_push
